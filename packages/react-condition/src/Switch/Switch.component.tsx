7a3b179c5c963315a509aa2b40fb975e55ac45c4
Alexey <Anisimov>
2020-12-02 18:37:03 +0700

    added switch component

---
+++ packages/react-condition/src/Switch/Switch.component.tsx
@@ -0,0 +1,116 @@
+import * as React from "react";
+
+function xc<T1>(arg: T1 | (() => T1)): T1 {
+  if (typeof arg === "function") {
+    return (arg as () => T1)();
+  }
+  return arg;
+}
+
+export const Case = ({
+  children,
+}: {
+  children?: (() => React.ReactNode) | React.ReactNode;
+  condition: (() => boolean) | boolean;
+  break?: boolean;
+}): JSX.Element => {
+  return (xc(children) || null) as JSX.Element;
+};
+
+export const Default = ({
+  children,
+}: {
+  children?: (() => React.ReactNode) | React.ReactNode;
+}): JSX.Element => {
+  return (xc(children) || null) as JSX.Element;
+};
+
+export function Switch({ children, multiple }: { children?: React.ReactNode; multiple?: boolean }) {
+  if (!children || children === null) {
+    return null;
+  }
+
+  if (
+    typeof children === "string" ||
+    typeof children === "number" ||
+    typeof children === "boolean"
+  ) {
+    return <>{children}</>;
+  }
+
+  const results: { element: React.ReactNode; type: "always" | "condition" | "default" }[] = [];
+  const BreakException = {};
+
+  try {
+    React.Children.forEach(children, (child) => {
+      if (!child || child === null) {
+        results.push({
+          element: null,
+          type: "always",
+        });
+      } else if (
+        typeof child === "string" ||
+        typeof child === "number" ||
+        typeof child === "boolean"
+      ) {
+        results.push({
+          element: child,
+          type: "always",
+        });
+      } else if ((child as any).type === Case) {
+        if (xc((child as any).props.condition)) {
+          results.push({
+            element: child,
+            type: "condition",
+          });
+        }
+        if (xc((child as any).props.break)) {
+          throw BreakException;
+        }
+      } else if ((child as any).type === Default) {
+        results.push({
+          element: child,
+          type: "default",
+        });
+      } else {
+        results.push({
+          element: child,
+          type: "always",
+        });
+      }
+    });
+  } catch (e) {
+    if (e !== BreakException) {
+      throw e;
+    }
+  }
+
+  let hasCase = false;
+  if (multiple) {
+    const result = results.map(({ element, type }) => {
+      if (type === "condition") {
+        hasCase = true;
+      }
+      return type !== "default" ? element : null;
+    });
+    if (result.length === 0 || !hasCase) {
+      return <>{results.map(({ element }) => element)}</>;
+    }
+    return <>{result}</>;
+  }
+
+  const result = results.map(({ element, type }) => {
+    const candidate = type !== "default" && !hasCase ? element : null;
+    if (type === "condition") {
+      hasCase = true;
+    }
+    return candidate;
+  });
+
+  if (result.length === 0 || !hasCase) {
+    return <>{results.map(({ element }) => element)}</>;
+  }
+  return <>{result}</>;
+}
+
+export default { Switch, Case, Default };
---
+++ packages/react-condition/src/Switch/Switch.test.tsx
@@ -0,0 +1,168 @@
+import * as React from "react";
+
+import { cleanup, render } from "@testing-library/react";
+
+import { Case, Default, Switch } from "../react-condition";
+
+describe("Switch component", () => {
+  afterEach(cleanup);
+
+  test("Render children without cases", () => {
+    const { container } = render(<Switch>Test string;</Switch>);
+    expect(container.innerHTML).toBe("Test string;");
+  });
+
+  test("Render null as a children", () => {
+    const { container } = render(<Switch />);
+    expect(container.innerHTML).toBe("");
+  });
+
+  test("Should render if case condition === true", () => {
+    const { container } = render(
+      <Switch>
+        Test string;
+        <Case condition>Right string;</Case>
+        <Case condition={false}>Wrong string;</Case>
+      </Switch>
+    );
+    expect(container.innerHTML).toBe("Test string;Right string;");
+  });
+
+  test("Case should accept function as child", () => {
+    const { container } = render(
+      <Switch>
+        Test string;
+        <Case condition>{() => "Right string;"}</Case>
+        <Case condition={false}>Wrong string;</Case>
+      </Switch>
+    );
+    expect(container.innerHTML).toBe("Test string;Right string;");
+  });
+
+  test("Should accept null as child", () => {
+    const { container } = render(
+      <Switch>
+        Test string;
+        <Case condition>{() => "Right string;"}</Case>
+        {null}
+        <Case condition={false}>Wrong string;</Case>
+      </Switch>
+    );
+    expect(container.innerHTML).toBe("Test string;Right string;");
+  });
+
+  test("Should render Default if there are no positive cases", () => {
+    const { container } = render(
+      <Switch>
+        <>Test string;</>
+        <Case condition={false}>Wrong string;</Case>
+        <Default>Right string;</Default>
+      </Switch>
+    );
+    expect(container.innerHTML).toBe("Test string;Right string;");
+  });
+
+  test("Should not render Default if there are positive cases", () => {
+    const { container } = render(
+      <Switch>
+        Test string;
+        <Case condition>Right string;</Case>
+        <Default>Wrong string;</Default>
+      </Switch>
+    );
+    expect(container.innerHTML).toBe("Test string;Right string;");
+  });
+
+  test("Should render only one case if condition === true", () => {
+    const { container } = render(
+      <Switch>
+        <>Test string;</>
+        <Case condition>Right string;</Case>
+        <Case condition>Wrong string;</Case>
+      </Switch>
+    );
+    expect(container.innerHTML).toBe("Test string;Right string;");
+  });
+
+  test("Should render only all positive cases, when multiple === true", () => {
+    const { container } = render(
+      <Switch multiple>
+        <>Test string;</>
+        <Case condition>Right string;</Case>
+        <Case condition={false}>Wrong string;</Case>
+        <Case condition>Also right string;</Case>
+      </Switch>
+    );
+    expect(container.innerHTML).toBe("Test string;Right string;Also right string;");
+  });
+
+  test("Should render Default if there are no positive cases when multiple === true", () => {
+    const { container } = render(
+      <Switch multiple>
+        <>Test string;</>
+        <Case condition={false}>Wrong string;</Case>
+        <Default>Right string;</Default>
+      </Switch>
+    );
+    expect(container.innerHTML).toBe("Test string;Right string;");
+  });
+
+  test("Should render all positive cases, when multiple === true, but not after case with break === true", () => {
+    const { container } = render(
+      <Switch multiple>
+        <>Test string;</>
+        <Case condition break>
+          Right string;
+        </Case>
+        <Case condition={false}>Wrong string;</Case>
+        <Case condition>Not right string;</Case>
+      </Switch>
+    );
+    expect(container.innerHTML).toBe("Test string;Right string;");
+  });
+
+  test("Should throw error outside", () => {
+    const renderWithInvalidCase = () =>
+      render(
+        <Switch multiple>
+          <>Test string;</>
+          <Case
+            condition={() => {
+              throw new Error("💣");
+            }}
+          >
+            Cant see me
+          </Case>
+        </Switch>
+      );
+    expect(renderWithInvalidCase).toThrow("💣");
+  });
+
+  test("Should render null if Case's children missing", () => {
+    const renderCase = jest.fn(() => undefined);
+    const { container } = render(
+      <Switch multiple>
+        <>Test string;</>
+        <Case condition break>
+          {() => renderCase()}
+        </Case>
+        <Case condition={false}>Wrong string;</Case>
+      </Switch>
+    );
+    expect(container.innerHTML).toBe("Test string;");
+    expect(renderCase).toBeCalled();
+  });
+
+  test("Should render null if Default's children missing", () => {
+    const renderDefault = jest.fn(() => undefined);
+    const { container } = render(
+      <Switch multiple>
+        <>Test string;</>
+        <Case condition={false}>Wrong string;</Case>
+        <Default>{() => renderDefault()}</Default>
+      </Switch>
+    );
+    expect(container.innerHTML).toBe("Test string;");
+    expect(renderDefault).toBeCalled();
+  });
+});
